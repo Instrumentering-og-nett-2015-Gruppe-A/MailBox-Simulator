778de912b910b6436fceffd823f377508dabbab0
theakt <theakt@stud.ntnu.no>
2015-02-11 14:31:38 +0100

    Oppretter fxml m/knapper

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,29 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setController(this);
+			Parent root = (Parent) fxmlLoader.load(this.getClass().getResourceAsStream("MailboxSimButtons.fxml"));
+			
+			newRfid.setDisable(true);
+			
+			Scene scene = new Scene(root,400,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -20,7 +32,44 @@ public class Main extends Application {
 		}
 	}
 	
+
+	@FXML
+	private CheckBox isPost = new CheckBox();
+	
+	@FXML
+	private Button insideButton = new Button();
+	
+	@FXML
+	private Button newRfid = new Button();
+	
+	@FXML
+	private Button wrongRfid = new Button();
+	
+	@FXML
+	private ComboBox<String> regRfid = new ComboBox<String>();
+	
+	
+	
+	@FXML public void insideBtnPushed(ActionEvent event){
+		newRfid.setDisable(false);
+	}
+	
+	@FXML public void scanNewRfid(ActionEvent event){
+	}
+	
+	@FXML public void scanRegRfid(ActionEvent event){
+	}
+	
+	@FXML public void scanWrong(ActionEvent event){
+	}
+	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+
+
+
+
+
